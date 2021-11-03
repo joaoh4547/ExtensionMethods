83d4b34c5fcd30ee02ba6255a7132ddbbb52910b
João Henrique <joaohenriquetdj@gmail.com>
2021-11-03 13:31:45 -0300

    Add StringExtensions

---
+++ ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Cut(this string thisString, int size)
+        {
+            if (thisString.Length < size)
+            {
+                throw new ArgumentException("the size cannot be greater than string size");
+            }
+
+            if (thisString.Length == size)
+            {
+                return thisString;
+            }
+
+            var newStr = thisString.Substring(0, size);
+
+            return newStr + "...";
+        }
+    }
+}
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -6,8 +6,19 @@ namespace ExtensionMethods
     {
         public static void Main(string[] args)
         {
+            // Example 01
+            Console.WriteLine("Example 01");
+            
             var dateTime = new DateTime(2021,11,03,10,24,12);
             Console.WriteLine(dateTime.ElapsedTime());
+            Console.WriteLine("---------------\n");
+         
+            
+            // Example 02
+            Console.WriteLine("Example 02");
+           
+            const string text = "Good morning";
+            Console.WriteLine(text.Cut(14));
         }
     }
 }   
