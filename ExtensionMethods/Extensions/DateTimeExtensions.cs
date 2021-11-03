d68edf7b46490602814b2c4f761c7050e4bd767a
João Henrique <joaohenriquetdj@gmail.com>
2021-11-03 13:10:33 -0300

    add DateTimeExtensions

---
+++ ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExtensionMethods.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisDateTime)
+        {
+            TimeSpan span = DateTime.Now.Subtract(thisDateTime);
+
+            if (span.TotalHours < 24.0)
+            {
+                return span.TotalHours.ToString("F1",CultureInfo.InvariantCulture) + " Horas";
+            }
+           
+            return span.TotalDays.ToString("F1",CultureInfo.InvariantCulture) + " Dias";
+            
+        }
+    }
+}
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace ExtensionMethods
+﻿using System;
+using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
 {
      class Program
     {
         public static void Main(string[] args)
         {
+            var dateTime = new DateTime(2021,11,03,10,24,12);
+            Console.WriteLine(dateTime.ElapsedTime());
         }
     }
-}
+}   
