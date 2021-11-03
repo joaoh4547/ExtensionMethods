b0296f5307d6d2f7d890d79dfdb51fcc91dfeb34
João Henrique <joaohenriquetdj@gmail.com>
2021-11-03 13:35:34 -0300

    modify cut of StringExtensions

--- ExtensionMethods/Extensions/StringExtensions.cs
+++ ExtensionMethods/Extensions/StringExtensions.cs
@@ -4,19 +4,13 @@
     {
         public static string Cut(this string thisString, int size)
         {
-            if (thisString.Length < size)
-            {
-                throw new ArgumentException("the size cannot be greater than string size");
-            }
-
-            if (thisString.Length == size)
+            if (thisString.Length <= size)
             {
                 return thisString;
             }
 
-            var newStr = thisString.Substring(0, size);
 
-            return newStr + "...";
+            return thisString.Substring(0, size) + "...";
         }
     }
 }
